05e5e6b734faa4c81a849b859103c21a9cb27ac2
mckonieczny <mc.konieczny@gmail.com>
2016-10-20 18:50:52 +0200

    heroku

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -16,8 +16,8 @@ public class Main {
 
     public static void main(String[] args) {
 
-        staticFileLocation("/public");
         port(getHerokuAssignedPort());
+        staticFileLocation("/public");
 
         get("/hello", (req, res) -> "Hello Heroku World");
 
