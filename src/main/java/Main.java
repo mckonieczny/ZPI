fc7e049da01fae9db024f3c6a1fc596e03bc4dce
mckonieczny <mc.konieczny@gmail.com>
2016-10-20 18:57:54 +0200

    refactor heroku

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,6 +4,7 @@ import spark.template.freemarker.FreeMarkerEngine;
 import java.util.HashMap;
 import java.util.Map;
 
+import static server.SparkUtils.getHerokuAssignedPort;
 import static server.SparkUtils.renderContent;
 import static spark.Spark.get;
 import static spark.Spark.port;
@@ -33,13 +34,7 @@ public class Main {
 
     }
 
-    static int getHerokuAssignedPort() {
-        ProcessBuilder processBuilder = new ProcessBuilder();
-        if (processBuilder.environment().get("PORT") != null) {
-            return Integer.parseInt(processBuilder.environment().get("PORT"));
-        }
-        return 4567; //return default port if heroku-port isn't set (i.e. on localhost)
-    }
+
 
 
 }
--- src/main/java/server/SparkUtils.java
+++ src/main/java/server/SparkUtils.java
@@ -16,9 +16,19 @@ public class SparkUtils {
     public static String renderContent(String htmlFile) {
         try {
             URL url = SparkUtils.class.getResource(htmlFile);
+            System.out.println(url.toString());
             Path path = Paths.get(url.toURI());
+            System.out.println(path.toString());
             return new String(Files.readAllBytes(path), Charset.defaultCharset());
         } catch (IOException | URISyntaxException e) {}
         return null;
+    }
+
+    public static int getHerokuAssignedPort() {
+        ProcessBuilder processBuilder = new ProcessBuilder();
+        if (processBuilder.environment().get("PORT") != null) {
+            return Integer.parseInt(processBuilder.environment().get("PORT"));
+        }
+        return 4567; //return default port if heroku-port isn't set (i.e. on localhost)
     }
 }
