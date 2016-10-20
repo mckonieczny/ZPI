42b6841797370cd9d0602bfe76d9e465226b6a4e
mckonieczny <mc.konieczny@gmail.com>
2016-10-20 19:09:04 +0200

    refactor spark utils na potrzeby zdeployowanej aplikacji

--- src/main/java/server/SparkUtils.java
+++ src/main/java/server/SparkUtils.java
@@ -13,12 +13,30 @@ import java.nio.file.Paths;
  */
 public class SparkUtils {
 
+    private static boolean DEPLOYED = false;
+    private static String DEPLOYED_RESOURCES_PATH = "/app/build/resources/main/html/";
+
     public static String renderContent(String htmlFile) {
+        if(isDeployed()) {
+            return renderDeployedContent(htmlFile);
+        } else {
+            return renderLocalContent(htmlFile);
+        }
+    }
+
+    private static String renderDeployedContent(String htmlFile) {
+        try {
+            URL url = new URL(DEPLOYED_RESOURCES_PATH + htmlFile);
+            Path path = Paths.get(url.toURI());
+            return new String(Files.readAllBytes(path), Charset.defaultCharset());
+        } catch (IOException | URISyntaxException e) {}
+        return null;
+    }
+
+    private static String renderLocalContent(String htmlFile) {
         try {
             URL url = SparkUtils.class.getResource(htmlFile);
-            System.out.println(url.toString());
             Path path = Paths.get(url.toURI());
-            System.out.println(path.toString());
             return new String(Files.readAllBytes(path), Charset.defaultCharset());
         } catch (IOException | URISyntaxException e) {}
         return null;
@@ -27,8 +45,13 @@ public class SparkUtils {
     public static int getHerokuAssignedPort() {
         ProcessBuilder processBuilder = new ProcessBuilder();
         if (processBuilder.environment().get("PORT") != null) {
+            DEPLOYED = true;
             return Integer.parseInt(processBuilder.environment().get("PORT"));
         }
         return 4567; //return default port if heroku-port isn't set (i.e. on localhost)
+    }
+
+    public static boolean isDeployed() {
+        return DEPLOYED;
     }
 }
