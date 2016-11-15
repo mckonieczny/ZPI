f3cd912c5bccb31e7539c717a413e8ed44f98960
mckonieczny <mc.konieczny@gmail.com>
2016-11-15 14:33:47 +0100

    opcja resetu bazy danych

--- src/main/java/Samples.java
+++ src/main/java/Samples.java
@@ -1,6 +1,10 @@
+import database.document.CardDocument;
+import database.document.DeckDocument;
 import database.document.UserDocument;
+import database.mongo.MongoUtils;
 import database.repository.CardRepository;
 import database.repository.DeckRepository;
+import database.repository.FavoriteRepository;
 import database.repository.UserRepository;
 import org.pac4j.sparkjava.SecurityFilter;
 import security.LoginHandler;
@@ -15,7 +19,6 @@ import static server.SparkUtils.renderContent;
 import static server.SparkUtils.templateEngine;
 import static spark.Redirect.Status.TEMPORARY_REDIRECT;
 import static spark.Spark.*;
-import static spark.Spark.post;
 
 /**
  * Created by DjKonik on 2016-11-13.
@@ -78,10 +81,52 @@ public class Samples {
             return new ModelAndView(model, "register.ftl");
         }, templateEngine());
 
+        get("/zaorajmibaze",(req, res) -> {
+
+            MongoUtils.dropDatabase();
+            initDB();
+
+            return renderContent("/html/index.html");
+        });
+
     }
 
     private static ModelAndView getReactPage() {
         Map<String, Object> model = new HashMap<>();
         return new ModelAndView(model, "index.ftl");
+    }
+
+    private static void initDB() {
+
+        UserRepository userDB = new UserRepository();
+        DeckRepository deckDB = new DeckRepository();
+        CardRepository cardDB = new CardRepository();
+        FavoriteRepository favDB = new FavoriteRepository();
+
+        UserDocument user1 = new UserDocument("admin", "admin");
+        UserDocument user2 = new UserDocument("pwd", "pwd");
+
+        userDB.save(user1);
+        userDB.save(user2);
+
+        DeckDocument deck1 = new DeckDocument(user1.getId(), "Owoce i warzywa", "Proste słówka, owoce i warzywa", 1);
+        DeckDocument deck2 = new DeckDocument(user1.getId(), "Sporty", "Proste słówka, dyscypliny sportowe", 1);
+        DeckDocument deck3 = new DeckDocument(user2.getId(), "Części do Ursusa", "Słownictwo fachowe, dla mechaników i serwisantów traktorów w Anglii", 5);
+
+        deckDB.save(deck1);
+        deckDB.save(deck2);
+        deckDB.save(deck3);
+
+        cardDB.save(new CardDocument(deck1.getId(),"alpha", "ahpla"));
+        cardDB.save(new CardDocument(deck2.getId(),"beta", "ateb"));
+        cardDB.save(new CardDocument(deck2.getId(),"gamma", "ammag"));
+        cardDB.save(new CardDocument(deck3.getId(),"epsilon", "nolispe"));
+        cardDB.save(new CardDocument(deck3.getId(),"lambda", "adbmal"));
+        cardDB.save(new CardDocument(deck3.getId(),"omega", "agemo"));
+
+        favDB.save(user1.getId(), deck1.getId());
+        favDB.save(user1.getId(), deck3.getId());
+        favDB.save(user2.getId(), deck1.getId());
+        favDB.save(user2.getId(), deck2.getId());
     }
 }
