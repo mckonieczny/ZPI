425c524b55abbbfdfcce808bcad4f200adf4ec3a
mckonieczny <mc.konieczny@gmail.com>
2016-11-16 20:36:10 +0100

    json body rest api favorites

--- src/main/java/controllers/FavoriteController.java
+++ src/main/java/controllers/FavoriteController.java
@@ -1,7 +1,11 @@
 package controllers;
 
+import database.document.FavoriteDocument;
 import database.repository.FavoriteRepository;
 
+import static java.net.HttpURLConnection.HTTP_BAD_REQUEST;
+import static java.net.HttpURLConnection.HTTP_OK;
+import static org.bson.Document.parse;
 import static security.LoginHandler.loggedUserId;
 import static server.SparkUtils.notEmpty;
 import static spark.Spark.post;
@@ -32,6 +36,31 @@ public class FavoriteController extends AbstractController {
                 return statusOk();
             } else {
                 return statusError();
+            }
+        });
+
+        post("/api/favorite/add2", (req, res) -> {
+            FavoriteDocument favorite = new FavoriteDocument(parse(req.body()));
+            favorite.setUserId(loggedUserId(req, res));
+            if (notEmpty(favorite.getDeckId())) {
+                favoriteRepository.save(favorite);
+                res.status(HTTP_OK);
+                return favorite.toJson();
+            } else {
+                res.status(HTTP_BAD_REQUEST);
+                return "";
+            }
+        });
+
+        post("/api/favorite/remove2", (req, res) -> {
+            FavoriteDocument favorite = new FavoriteDocument(parse(req.body()));
+            if (notEmpty(favorite.getDeckId())) {
+                favoriteRepository.delete(favorite.getId(), loggedUserId(req, res));
+                res.status(HTTP_OK);
+                return "";
+            } else {
+                res.status(HTTP_BAD_REQUEST);
+                return "";
             }
         });
     }
